dac74a917bfded79f51a690cf9f3ac431f58850a
jskelcy <jskelcy@gmail.com>
2014-02-18 18:06:15 -0500

    problems updating newLiving and newDead

--- life.js
+++ life.js
@@ -11,6 +11,7 @@ var World = function(){
 			this.grid[i][j] =0;
 		}
 	}
+
 } 
 
 World.prototype.toString = function() {
@@ -24,9 +25,60 @@ World.prototype.toString = function() {
 }
 
 World.prototype.update = function () {
-	
+	console.log("idk");
+	var newLiving = Array();
+	var newDead = Array();
+	for(var i =0; i<this.HEIGHT; i++){
+		for(var j = 0; j<this.WIDTH; j++){
+			//this moves through the entire grid
+			var currNode = this.grid[i][j];
+			var livingNeighbors =0;
+			//this nested for loop with traverse the 8 nodes around the currNode
+			for(var k = i-1; k<=i+1; k++){
+				for(var m = j-1; m<=j+1;m++){
+					//this looks for adj node
+					if((k <0) || (m<0) || (k>=this.HEIGHT) || (m>=this.WIDTH) ){
+						continue;
+					}
+					livingNeighbors += this.grid[k][m];
+					if(currNode){
+						livingNeighbors--;
+					}
+				}
+			}
+			//update life of cell
+			if(currNode){
+				if((livingNeighbors ==1) || (livingNeighbors>3)){
+					newDead.push([i, j]);
+				}
+			}else{
+				if(livingNeighbors ==3) {
+					newLiving.push([i, j]);
+				}
+			}
+		}
+	}	
+	console.log("newDead.length = " + newDead.length)
+	console.log("newDead.length = " + newLiving.length)
+	for (var i = 0; i < newDead.length; i++) {
+		var ij = newDead[i];
+		console.log("i, j = " + ij[0] + ", " + ij[1]);
+		this.grid[ij[0]][ij[1]] = 0;
+	}
+	for (var i = 0; i < newLiving.length; i++) {
+		console.log(ij);
+		var ij = newLiving[i];
+		this.grid[ij[0]][ij[1]] = 0;
+	}
 }
 
 
 var game = new World();
+game.grid[4][4] = 1;
+game.grid[4][5] = 1;
+game.grid[4][6] = 1;
+
 	
+console.log(game.toString());
+game.update();
+console.log(game.toString());
